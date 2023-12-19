92a95eecc7f89717925ec33ae3e3d97ac0378613
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2023-12-18 19:10:15 -0500

    Add freetype binary git submodule to replace vcpkg

---
+++ lib/FindFreetype.cmake
@@ -0,0 +1,2 @@
+set(FREETYPE_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/lib/freetype-windows-binaries/include)
+set(FREETYPE_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/freetype-windows-binaries/release static/vs2015-2022/win64/freetype.lib")
