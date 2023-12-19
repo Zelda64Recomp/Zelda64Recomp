32cb89ed76a466131b916e718f1144f06df4ae6b
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2023-12-18 19:26:47 -0500

    Added MM decomp as submodule and added workaround file to avoid needing to build it

---
+++ patches/code/sub_s/sub_s.h
@@ -0,0 +1,10 @@
+// Required to include MM decomp headers without having built the repo
+
+#ifndef SUB_S_H
+#define SUB_S_H 1
+
+extern Vtx codeVtx_120260[];
+extern Gfx gShadowMaterialDL[];
+extern Gfx gShadowModelDL[];
+
+#endif
