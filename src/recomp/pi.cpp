0de47dedc20647659094a799d39ada85acfcadfc
angie <angheloalf95@gmail.com>
2024-05-14 10:44:34 -0400

    Use `size_t` for iterator in `save_write`

--- src/recomp/pi.cpp
+++ src/recomp/pi.cpp
@@ -150,7 +150,7 @@ void save_write_ptr(const void* in, uint32_t offset, uint32_t count) {
 void save_write(RDRAM_ARG PTR(void) rdram_address, uint32_t offset, uint32_t count) {
     {
         std::lock_guard lock { save_context.save_buffer_mutex };
-        for (uint32_t i = 0; i < count; i++) {
+        for (size_t i = 0; i < count; i++) {
             save_context.save_buffer[offset + i] = MEM_B(i, rdram_address);
         }
     }
