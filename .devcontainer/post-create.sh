d8668e49e1a105ccadad4b607fd22f15c2f83834
David Chavez <david@dcvz.io>
2024-05-19 14:11:45 +0000

    Create local symlinks for N64Recomp

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -14,5 +14,10 @@ RSPRecomp njpgdspMain.us.rev1.toml
 # Cleanup 
 rm -rf zre
 
+# symlink recomp tools to root (CMake needs them there)
+# should be improved in the future
+ln -s $(which N64Recomp) .
+ln -s $(which RSPRecomp) .
+
 # Initialize submodules
 git submodule update --init --recursive
