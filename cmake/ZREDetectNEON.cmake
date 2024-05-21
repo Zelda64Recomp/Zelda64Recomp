0f4db23fe026ba3c63c0de58f483175d3323ae8b
dcvz <david@dcvz.io>
2024-05-22 00:52:49 +0200

    Allow overriding via cmd

--- cmake/ZREDetectNEON.cmake
+++ cmake/ZREDetectNEON.cmake
@@ -1,4 +1,8 @@
-try_compile(SUPPORTS_NEON
-  "${CMAKE_BINARY_DIR}"
-  "${CMAKE_SOURCE_DIR}/cmake/checks/cpu_neon.cpp"
-)
+if(NOT DEFINED SUPPORTS_NEON)
+  try_compile(SUPPORTS_NEON
+    "${CMAKE_BINARY_DIR}"
+    "${CMAKE_SOURCE_DIR}/cmake/checks/cpu_neon.cpp"
+  )
+endif()
+
+message(STATUS "SUPPORTS NEON: ${SUPPORTS_NEON}")
--- cmake/ZREDetectSSE42.cmake
+++ cmake/ZREDetectSSE42.cmake
@@ -1,4 +1,8 @@
-try_compile(SUPPORTS_SSE42
-  "${CMAKE_BINARY_DIR}"
-  "${CMAKE_SOURCE_DIR}/cmake/checks/cpu_sse42.cpp"
-)
+if(NOT DEFINED SUPPORTS_SSE42)
+  try_compile(SUPPORTS_SSE42
+    "${CMAKE_BINARY_DIR}"
+    "${CMAKE_SOURCE_DIR}/cmake/checks/cpu_sse42.cpp"
+  )
+endif()
+
+message(STATUS "SUPPORTS SSE4.2 = ${SUPPORTS_SSE42}")
