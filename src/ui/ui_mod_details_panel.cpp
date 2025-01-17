1e91bddeff42f86f6897bb0dc8b289b74c39f561
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-01-16 23:53:10 -0500

    Layout for mod details panel, add gap property setters

--- src/ui/ui_mod_details_panel.cpp
+++ src/ui/ui_mod_details_panel.cpp
@@ -7,15 +7,19 @@ namespace recompui {
 ModDetailsPanel::ModDetailsPanel(Element *parent) : Element(parent) {
     set_flex(1.0f, 1.0f, 200.0f);
     set_height(100.0f, Unit::Percent);
+    set_display(Display::Flex);
     set_flex_direction(FlexDirection::Column);
     set_border_bottom_right_radius(16.0f);
     set_background_color(Color{ 190, 184, 219, 25 });
 
     header_container = new Container(FlexDirection::Row, JustifyContent::FlexStart, this);
+    header_container->set_flex(0.0f, 0.0f);
     header_container->set_padding(16.0f);
+    header_container->set_gap(16.0f);
     header_container->set_background_color(Color{ 0, 0, 0, 89 });
     {
         thumbnail_container = new Container(FlexDirection::Column, JustifyContent::SpaceEvenly, header_container);
+        thumbnail_container->set_flex(0.0f, 0.0f);
         {
             thumbnail_image = new Image(thumbnail_container);
             thumbnail_image->set_width(100.0f);
@@ -24,10 +28,9 @@ ModDetailsPanel::ModDetailsPanel(Element *parent) : Element(parent) {
         }
 
         header_details_container = new Container(FlexDirection::Column, JustifyContent::SpaceEvenly, header_container);
-        header_details_container->set_width(100.0f, Unit::Percent);
-        header_details_container->set_margin_left(32.0f);
-        header_details_container->set_overflow(Overflow::Hidden);
-
+        header_details_container->set_flex(1.0f, 1.0f);
+        header_details_container->set_flex_basis(100.0f, Unit::Percent);
+        header_details_container->set_text_align(TextAlign::Left);
         {
             title_label = new Label(LabelStyle::Large, header_details_container);
             version_label = new Label(LabelStyle::Normal, header_details_container);
@@ -35,19 +38,26 @@ ModDetailsPanel::ModDetailsPanel(Element *parent) : Element(parent) {
     }
 
     body_container = new recompui::Container(FlexDirection::Column, JustifyContent::FlexStart, this);
-    body_container->set_padding_left(16.0f);
+    body_container->set_flex(0.0f, 0.0f);
+    body_container->set_text_align(TextAlign::Left);
+    body_container->set_padding(16.0f);
+    body_container->set_gap(16.0f);
     {
         description_label = new Label(LabelStyle::Normal, body_container);
         authors_label = new Label(LabelStyle::Normal, body_container);
-        spacer_element = new Element(body_container);
-        buttons_container = new Container(FlexDirection::Row, JustifyContent::SpaceAround, body_container);
-        buttons_container->set_padding_left(16.0f);
-        {
-            enable_toggle = new Toggle(buttons_container);
-            enable_toggle->add_checked_callback(std::bind(&ModDetailsPanel::enable_toggle_checked, this, std::placeholders::_1));
-            configure_button = new Button("Configure", recompui::ButtonStyle::Secondary, buttons_container);
-            erase_button = new Button("Erase", recompui::ButtonStyle::Secondary, buttons_container);
-        }
+    }
+    
+    spacer_element = new Element(this);
+    spacer_element->set_flex(1.0f, 0.0f);
+    
+    buttons_container = new Container(FlexDirection::Row, JustifyContent::SpaceAround, this);
+    buttons_container->set_flex(0.0f, 0.0f);
+    buttons_container->set_padding(16.0f);
+    {
+        enable_toggle = new Toggle(buttons_container);
+        enable_toggle->add_checked_callback(std::bind(&ModDetailsPanel::enable_toggle_checked, this, std::placeholders::_1));
+        configure_button = new Button("Configure", recompui::ButtonStyle::Secondary, buttons_container);
+        erase_button = new Button("Erase", recompui::ButtonStyle::Secondary, buttons_container);
     }
 }
 
