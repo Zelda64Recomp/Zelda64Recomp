0700c8fdc3ff64799524e777b5b9516c0cde7788
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-01-25 21:53:27 -0500

    Update runtime and implement ROM decompression for function hooking

--- src/main/register_patches.cpp
+++ src/main/register_patches.cpp
@@ -9,4 +9,5 @@ void zelda64::register_patches() {
     recomp::overlays::register_patches(mm_patches_bin, sizeof(mm_patches_bin), section_table, ARRLEN(section_table));
     recomp::overlays::register_base_exports(export_table);
     recomp::overlays::register_base_events(event_names);
+    recomp::overlays::register_manual_patch_symbols(manual_patch_symbols);
 }
