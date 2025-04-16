mohammed-addar/Zelda64RecompTst

Use custom controller db mappings

.github/linux/appimage.sh
@@ -20,7 +20,7 @@ chmod a+x linuxdeploy*
 mkdir -p AppDir/usr/bin
 cp Zelda64Recompiled AppDir/usr/bin/
 cp -r assets/ AppDir/usr/bin/
-cp gamecontrollerdb.txt AppDir/usr/bin/
+cp zeldacontrollerdb.txt AppDir/usr/bin/
 cp icons/512.png AppDir/Zelda64Recompiled.png
 cp .github/linux/Zelda64Recompiled.desktop AppDir/
 

src/main/main.cpp
@@ -457,7 +457,7 @@ int main(int argc, char** argv) {
     reset_audio(48000);
 
     // Source controller mappings file
-    if (SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt") < 0) {
+    if (SDL_GameControllerAddMappingsFromFile("zeldacontrollerdb.txt") < 0) {
         fprintf(stderr, "Failed to load controller mappings: %s\n", SDL_GetError());
     }
 

Zelda64Recomp/Zelda64Recomp

.github/linux/appimage.sh
@@ -20,7 +20,7 @@ chmod a+x linuxdeploy*
 mkdir -p AppDir/usr/bin
 cp Zelda64Recompiled AppDir/usr/bin/
 cp -r assets/ AppDir/usr/bin/
-cp gamecontrollerdb.txt AppDir/usr/bin/
+cp recompcontrollerdb.txt AppDir/usr/bin/
 cp icons/512.png AppDir/Zelda64Recompiled.png
 cp .github/linux/Zelda64Recompiled.desktop AppDir/
 

src/main/main.cpp
@@ -603,7 +603,7 @@ int main(int argc, char** argv) {
     reset_audio(48000);
 
     // Source controller mappings file
-    if (SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt") < 0) {
+    if (SDL_GameControllerAddMappingsFromFile("recompcontrollerdb.txt") < 0) {
         fprintf(stderr, "Failed to load controller mappings: %s\n", SDL_GetError());
     }
 