64e4d91e13d8589c61326c6ba9a598620a602a04
thecozies <79979276+thecozies@users.noreply.github.com>
2025-05-01 09:39:53 -0500

    add focus to text input

--- src/ui/elements/ui_text_input.cpp
+++ src/ui/elements/ui_text_input.cpp
@@ -28,7 +28,7 @@ namespace recompui {
         }
     }
     
-    TextInput::TextInput(Element *parent, bool text_visible) : Element(parent, Events(EventType::Text), "input") {
+    TextInput::TextInput(Element *parent, bool text_visible) : Element(parent, Events(EventType::Text, EventType::Focus), "input") {
         if (!text_visible) {
             set_attribute("type", "password");
         }
