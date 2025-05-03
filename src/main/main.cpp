0ee273eb2285b5c19b7ae880d19e0c00ad2e96bc
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-05-03 19:14:32 -0400

    Bump version to 1.2.0-rc8

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -48,7 +48,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "1.2.0-rc7";
+const std::string version_string = "1.2.0-rc8";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
@@ -724,4 +724,3 @@ int main(int argc, char** argv) {
 
     return EXIT_SUCCESS;
 }
-
