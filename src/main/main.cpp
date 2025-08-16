cac84f4216f00732f03ecc2bd9fac18360795d75
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-08-15 21:50:00 -0400

    Bump version number for 1.2.1 release candidate 1

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -56,7 +56,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "1.2.1-dev";
+const std::string version_string = "1.2.1-rc1";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
