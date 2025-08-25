549af0ac497fb7527df90b1752430684f3f0ea71
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-08-25 00:20:04 -0400

    Update RT64 for multimonitor frame pacing issue and bump version number

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -56,7 +56,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "1.2.1";
+const std::string version_string = "1.2.2-dev";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
